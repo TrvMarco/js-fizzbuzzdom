d8f3f3564ea0cfc4619c6b8bb281187435265375
TrvMarco <trevisanello.marco@gmail.com>
2022-05-02 20:29:11 +0200

    abbozzato codice js  e classi css

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
+const container = document.querySelector(".container")
+
 // SCRIVERE PROGRAMMA CHE STAMPI IN CONSOLE NUMERI DA 1 A 100
 for(let i = 1; i <= 100; i++){
-    if(i % 3 == 0 && i % 5 == 0){
-        console.log('FizzBuzz')
-    }else if(i % 5 == 0){
-        console.log('Buzz')
-    }if(i % 3 == 0){
-            console.log('Fizz')
+    const element = document.createElement("div");
+    element.classList.add("box");
+    element.innerText = i;  
+    container.append(element);
+    if(i % 3 === 0 && i % 5 === 0){
+        element.innerText = "fizzbuzz";
+        element.classList.add("fizzbuzz");
+        console.log('FizzBuzz');
+    }else if(i % 5 === 0){
+        element.innerText = "buzz";
+        element.classList.add("buzz");
+        console.log('Buzz');
+    }if(i % 3 === 0){
+            element.innerText = "fizz";
+            element.classList.add("fizz");
+            console.log('Fizz');
     }else{
-        console.log(i)
+        element.classList.add("standard");
+        console.log(i);
     };
 };
